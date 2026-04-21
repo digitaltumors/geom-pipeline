7a25c1b1af48ef3202ad3309823e49cc2a5b074c
Michael Tran <mtran256@uwo.ca>
2026-04-21 15:45:23 -0400

    style: run ruff formatter

--- workflow/names.py
+++ workflow/names.py
@@ -1,3 +1,1 @@
-
-
-#https://annotationdb.bhklab.ca/compound/many?compounds=59174488&format=json&bioassay=false&mechanism=false&toxicity=false
+# https://annotationdb.bhklab.ca/compound/many?compounds=59174488&format=json&bioassay=false&mechanism=false&toxicity=false
